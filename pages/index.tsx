1d0bc8baa1f31fe305857b66211a1ab3312bb63b
Ilija Radivojevic <ilija.radivojevic237@gmail.com>
2023-08-28 21:18:58 +0200

    First commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,161 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Bars2Icon, ChevronDownIcon } from "@heroicons/react/20/solid";
+import {
+  ChevronUpDownIcon,
+  PlusSmallIcon,
+  EllipsisHorizontalIcon,
+} from "@heroicons/react/24/outline";
+import { UserIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+    <div className="bg-white w-full md:bg-gray-50 min-h-screen flex flex-col justify-center">
+      <header className="md:py-4 w-full md:px-9 border-b border-gray-200 md:border-transparent max-w-5xl mx-auto md:mt-5 py-2.5 pl-6 pr-4 flex items-center justify-between">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          height="28"
+          width="102"
+          src="/logo.svg"
+          alt="This is logo nigga"
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+        <nav className="hidden md:block">
+          <ul className="flex gap-x-9">
+            <li className="text-sm/6 font-medium text-gray-800">
+              <Link href="#">Overview</Link>
+            </li>
+            <li className="text-sm/6 font-medium text-gray-800">
+              <Link href="#">Schedules</Link>
+            </li>
+            <li className="text-sm/6 font-medium text-gray-800">
+              <Link href="#">Changes</Link>
+            </li>
+            <li className="text-sm/6 font-medium text-gray-800">
+              <Link href="#">Help</Link>
+            </li>
+          </ul>
+        </nav>
+        <button className="gap-x-1 items-center hidden md:flex">
+          <div className="rounded-full border shrink-0 border-gray-300 bg-white h-10 w-10 flex items-center justify-center">
+            <UserIcon className="text-gray-400 w-5 h-5 " />
+          </div>
+          <ChevronUpDownIcon className="text-gray-400 w-5 h-5" />
+        </button>
+        <button className="md:hidden h-10 w-10 flex items-center justify-center">
+          <Bars2Icon className="text-gray-500 w-6 h-6" />
+        </button>
+      </header>
+      <main className="overflow-hidden grow w-full md:mt-9 mx-auto max-w-7xl rounded-t-3xl border border-gray-900/5 bg-white shadow-sm">
+        <hgroup className="mt-8 md:mt-10 px-6 md:px-0">
+          <h1 className="text-lg md:text-xl text-gray-800 font-medium md:text-center">
+            Welcome back, Nigga
+          </h1>
+          <p className="text-gray-600 text-sm/6 md:mx-auto mt-3 md:text-center md:max-w-md">
+            Stay organized and on top of your responsibilities with a
+            comprehensive view of all your tasks.
           </p>
-        </a>
+        </hgroup>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+        <div className="mt-8 md:mt-16 border-y border-gray-100 flex justify-between">
+          <div className="md:ml-8 flex -mb-px">
+            <button className="px-7 py-4 font-semibold text-sm/6 text-gray-800 border-b border-gray-500">
+              All Tasks
+            </button>
+            <button className="px-7 py-4 font-medium text-sm/6 text-gray-500 border-b border-transparent">
+              Work
+            </button>
+          </div>
+          <div className="hidden md:flex mr-12 gap-x-9">
+            <button className="flex items-center gap-x-1.5">
+              <PlusSmallIcon className="w-4 h-4 text-gray-500" />
+              <span className="text-gray-900 text-sm/6">New category</span>
+            </button>
+            <button className="flex items-center gap-x-1.5">
+              <span className="text-gray-900 text-sm/6">Sort by</span>
+              <ChevronDownIcon className="w-4 h-4 text-gray-500" />
+            </button>
+          </div>
+          <button className="mr-2.5 md:hidden p-3.5">
+            <EllipsisHorizontalIcon className="h-6 w-6 text-gray-600" />
+          </button>
+        </div>
+        <ul className="divide-y divide-gray-100 text-sm/6 overflow-x-auto">
+          <li className="flex justify-between h-14 items-center whitespace-nowrap gap-x-12 w-full">
+            <div className="text-gray-800 font-medium ml-8">
+              1. Update the CSS styles for the homepage banner.
+            </div>
+            <div className="flex gap-x-6 items-center justify-end min-w-[220px] pr-12">
+              <div className="bg-red-500/[0.15] font-medium text-red-700 rounded-full py-0.5 px-3">
+                High
+              </div>
+              <div className="text-gray-600">28-Aug-23</div>
+            </div>
+          </li>
+          <li className="flex justify-between h-14 items-center whitespace-nowrap gap-x-12 w-full">
+            <div className="text-gray-800 font-medium ml-8">
+              1. Update the CSS styles for the homepage banner.
+            </div>
+            <div className="flex gap-x-6 items-center justify-end min-w-[220px] pr-12">
+              <div className="bg-yellow-500/[0.15] font-medium text-yellow-600 rounded-full py-0.5 px-3">
+                Medium
+              </div>
+              <div className="text-gray-600">28-Aug-23</div>
+            </div>
+          </li>
+          <li className="flex justify-between h-14 items-center whitespace-nowrap gap-x-12 w-full">
+            <div className="text-gray-800 font-medium ml-8">
+              1. Update the CSS styles for the homepage banner.
+            </div>
+            <div className="flex gap-x-6 items-center justify-end min-w-[220px] pr-12">
+              <div className="bg-gray-100 font-medium text-gray-600 rounded-full py-0.5 px-3">
+                Low
+              </div>
+              <div className="text-gray-600">28-Aug-23</div>
+            </div>
+          </li>
+          <li className="flex justify-between h-14 items-center whitespace-nowrap gap-x-12 w-full">
+            <div className="text-gray-800 font-medium ml-8">
+              1. Update the CSS styles for the homepage banner.
+            </div>
+            <div className="flex gap-x-6 items-center justify-end min-w-[220px] pr-12">
+              <div className="text-gray-600">28-Aug-23</div>
+            </div>
+          </li>
+          <li className="flex justify-between h-14 items-center whitespace-nowrap gap-x-12 w-full">
+            <div className="text-gray-800 font-medium ml-8">
+              1. Update the CSS styles for the homepage banner.
+            </div>
+            <div className="flex gap-x-6 items-center justify-end min-w-[220px] pr-12">
+              <div className="text-gray-600">28-Aug-23</div>
+            </div>
+          </li>
+          <li className="flex justify-between h-14 items-center whitespace-nowrap gap-x-12 w-full">
+            <div className="text-gray-800 font-medium ml-8">
+              1. Update the CSS styles for the homepage banner.
+            </div>
+            <div className="flex gap-x-6 items-center justify-end min-w-[220px] pr-12">
+              <div className="text-gray-600">28-Aug-23</div>
+            </div>
+          </li>
+        </ul>
+        <div className="border-y border-gray-100">
+          <div className="relative flex items-center">
+            <div className="absolute top-[22px] left-6">
+              <PlusSmallIcon className="h-5 w-5 text-gray-500" />
+            </div>
+            <input
+              type="text"
+              placeholder="Add new task to the category"
+              value=""
+              className="w-full focus:outline-none block py-5 pl-12 placeholder:text-gray-500 placeholder:text-sm/6"
+            />
+            <div className="absolute top-[18px] left-[264px] hidden md:block">
+              <div className="border border-gray-100 rounded-md px-3 py-1 text-xs/5">
+                Control + Space
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
 }
